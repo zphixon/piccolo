4d383b1173cbb5afc3b9c867d29d33a1c0a29989
Zack <zackh@firemail.cc>
2017-12-10 13:28:43 -0500

    update main

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -6,30 +6,37 @@ fn main() {
     //let code = "false == false";
     //let code = "\"strang\" == \"string\"";
     //let code = "\"string\" == \"string\"";
-    let code = "me 32 + 32\n\nme true\n\n\nme \"it is wednesday,\\nmy dudes\"\n";
+    //let code = "me 32 + 32\n\nme true\n\n\nme \"it is wednesday, my dudes\"\n";
+    let code = "test = 32\n\nme 33 + 24";
 
     let s = piccolo::scanner::Scanner::new(code.into()).scan_tokens();
 
     if s.is_err() {
-        println!("{}", s.err().unwrap());
+        println!("scan err\n\n{}", s.err().unwrap());
     } else {
+        println!("tokens:");
         for tok in s.clone().unwrap() {
             println!("{:?}", tok);
         }
+        println!();
 
         let p = piccolo::parser::Parser::new(s.unwrap()).parse();
 
         if p.is_err() {
-            println!("{}", p.err().unwrap());
+            println!("parse err\\n{}", p.err().unwrap());
         } else {
+            println!("statements:");
             for stmt in p.clone().unwrap() {
                 println!("{:?}", stmt);
             }
 
+            println!("\noutput:");
             let i = piccolo::interp::Interpreter::new().interpret(p.unwrap());
 
+            println!();
+
             if i.is_err() {
-                println!("{}", i.err().unwrap());
+                println!("runtime err\n\n{}", i.err().unwrap());
             } else {
                 println!("huzzah");
             }
