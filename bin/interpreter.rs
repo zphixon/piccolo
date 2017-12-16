f4a19db56f9945b778ac6894eed812f8976e1a11
Zack <zackh@firemail.cc>
2017-12-16 17:13:32 -0500

    me -> prln

--- bin/interpreter.rs
+++ bin/interpreter.rs
@@ -7,23 +7,23 @@ fn main() {
         //"false == false";
         //"\"strang\" == \"string\"";
         //"\"string\" == \"string\"";
-        //"me 32 + 32\n\nme true\n\n\nme \"it is wednesday, my dudes\"\n";
-        //"a = 0.1\n\nb=0.2\nme a + b == 0.3\na = 9\nme a + b";
+        //"prln(32 + 32)\n\nprln(true)\n\n\nprln(\"it is wednesday, my dudes\")\n";
+        //"a = 0.1\n\nb=0.2\nprln(a + b == 0.3)\na = 9\nprln(a + b)";
         //"a = 2 b = 3 a b = 4";
-        //"me a = 2";
+        //"prln(a = 2)";
         //"x = \"yes\"\nx or or or or";
-        //"a = 1\nb = 1\nc = 1\ndo\n  a = 2\n  b = 2\n  do\n    a = 3\n    me a\n    me b\n    me c\n  end\n  me a\n  me b\n  me c\nend\nme a\nme b\nme c\n";
-        //"x = true\nif x do\n  me \"hey, not bad\"\nend\n";
-        //"x = nil\nif x do\n  me \"crepe\"\nelse\n  me \"no crepe\"\nend\n";
-        //"i = 0\nwhile i < 10 do\n  i = i + 1\n  me i\nend\n";
-        //"arr = [8, 6, 7, 5, 3, 0, 9]\nfor num in arr do\n  me num\nend\n";
-        //"for i in 2...4 do\n  me i\nend";
-        //"for i in 4..5..6 do\n  me i\nend\n";
-        //"x = 1...10\n\nfor i in x do\n  me (i * 29) % 34\nend\n";
-        //"a = 0\nb = 1\n\nwhile a < 10000 do\n  me a\n  tmp = a\n  a = b\n  b = tmp + b\nend\n";
+        //"a = 1\nb = 1\nc = 1\ndo\n  a = 2\n  b = 2\n  do\n    a = 3\n    prln(a)\n    prln(b)\n    prln(c)\n  end\n  prln(a)\n  prln(b)\n  prln(c)\nend\nprln(a)\nprln(b)\nprln(c)\n";
+        //"x = true\nif x do\n  prln(\"hey, not bad\")\nend\n";
+        //"x = nil\nif x do\n  prln(\"crepe\")\nelse\n  prln(\"no crepe\")\nend\n";
+        //"i = 0\nwhile i < 10 do\n  i = i + 1\n  prln(i)\nend\n";
+        //"arr = [8, 6, 7, 5, 3, 0, 9]\nfor num in arr do\n  prln(num)\nend\n";
+        //"for i in 2...4 do\n  prln(i)\nend";
+        //"for i in 4..5..6 do\n  prln(i)\nend\n";
+        //"x = 1...10\n\nfor i in x do\n  prln((i * 29) % 34)\nend\n";
+        //"a = 0\nb = 1\n\nwhile a < 10000 do\n  prln(a)\n  tmp = a\n  a = b\n  b = tmp + b\nend\n";
         //"b = 6\nb b b b b";
-        //"me clock()\n";
-        "print(\"testarino\")";
+        //"prln(clock())\n";
+        "prln(\"testarino\")";
 
     println!("program:");
     for (k, v) in code.lines().enumerate() {
