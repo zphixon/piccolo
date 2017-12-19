94a7778cea79140d1990f98385598f41dce3e37b
Zack <zackh@firemail.cc>
2017-12-19 15:29:15 -0500

    separate env innards from outards
    maybe not necessary? not entirely sure, I'm still basically copying from rulox

--- src/env.rs
+++ src/env.rs
@@ -5,46 +5,74 @@ use ::*;
 
 use std::collections::HashMap;
 use std::rc::Rc;
+use std::cell::RefMut;
 use std::cell::RefCell;
 
 #[derive(Debug, Clone, PartialEq, Default)]
+struct EnvInner {
+    inner: Vec<HashMap<String, Rc<RefCell<value::Value>>>>,
+}
+
+#[derive(Debug, Clone, PartialEq, Default)]
 pub struct Env {
-    pub inner: Vec<HashMap<String, Rc<RefCell<value::Value>>>>,
-    pub splits: Vec<usize>,
+    inner: Rc<RefCell<EnvInner>>
+    //pub inner: Vec<HashMap<String, Rc<RefCell<value::Value>>>>,
+    //pub splits: Vec<usize>,
 }
 
 impl Env {
     pub fn new() -> Self {
+        let inner = Rc::new(RefCell::new(EnvInner {
+            inner: vec![HashMap::new()]
+        }));
+
+        Env { inner }
+    }
+
+    pub fn with_parent(parent: &Env) -> Self {
         Env {
-            inner: vec![HashMap::new()],
-            splits: vec![]
+            inner: parent.inner.clone()
         }
+        //let inner = Rc::new(RefCell::new(EnvInner {
+        //    inner: parent.inner.clone(),
+        //}));
+        //Env { inner }
+        //let mut e = Env {
+            //inner: parent.inner,
+            ////splits: vec![]
+        //};
+        //e.push();
+        //e
     }
 
-    pub fn with_parent(parent: Env) -> Self {
-        let mut e = Env {
-            inner: parent.inner,
-            splits: vec![]
-        };
-        e.push();
-        e
+    pub fn push(&self) {
+        let mut inner = self.inner.borrow_mut();
+        inner.inner.push(HashMap::new());
     }
 
-    pub fn push(&mut self) {
-        self.inner.push(HashMap::new());
+    //pub fn push_parent(&mut self, parent: Env) {
+    //    //self.splits.push(parent.inner.len());
+    //    //let mut old = std::mem::replace(&mut self.inner, parent.inner);
+    //    //self.inner.append(&mut old);
+    //}
+
+    pub fn pop(&self) {
+        self.inner.borrow_mut().inner.pop();
     }
 
-    pub fn push_parent(&mut self, parent: Env) {
-        self.splits.push(parent.inner.len());
-        let mut old = std::mem::replace(&mut self.inner, parent.inner);
-        self.inner.append(&mut old);
-    }
+    pub fn set(&self, name: &str, value: Rc<RefCell<value::Value>>) {
+        let mut inner = self.inner.borrow_mut();
+        for scope in inner.inner.iter_mut().rev() {
+            if scope.contains_key(name) {
+                scope.insert(name.to_owned(), value);
+                return
+            }
+        }
 
-    pub fn pop(&mut self) -> Option<HashMap<String, Rc<RefCell<value::Value>>>> {
-        self.inner.pop()
-    }
-
-    pub fn define(&mut self, name: &str, value: Rc<RefCell<value::Value>>) {
+        inner.inner.iter_mut().rev().nth(0)
+            .map(|m| m.insert(name.to_owned(), value))
+            .expect("empty env");
+        //self.inner.borrow_mut().inner
         /*if name == "counter" {
             println!("found");
             backtrace::trace(|frame| {
@@ -69,7 +97,7 @@ impl Env {
             });
         }*/
 
-        for scope in self.inner.iter_mut().rev().skip(1) {
+        /*for scope in self.inner.iter_mut().rev().skip(1) {
             if scope.contains_key(name) {
                 scope.insert(name.to_owned(), value);
                 return
@@ -78,46 +106,51 @@ impl Env {
 
         self.inner.iter_mut().rev().nth(0)
             .map(|m| m.insert(name.to_owned(), value))
-            .expect("env is empty");
+            .expect("env is empty");*/
     }
 
     pub fn get(&self, name: &str) -> Option<Rc<RefCell<value::Value>>> {
-        for scope in self.inner.iter().rev() {
+        for scope in self.inner.borrow().inner.iter().rev() {
             if scope.contains_key(name) {
-                return scope.get(name).cloned()
+                if let Some(r) = scope.get(name) {
+                    return Some(r.clone())
+                }
+                //let g: Option<&Rc<RefCell<value::Value>>> = scope.get(name);
+                //return Rc::get_mut(scope.get(name))
             }
         }
         None
     }
 
     pub fn set_local(&mut self, name: &str, value: Rc<RefCell<value::Value>>)  {
-        self.inner.iter_mut().rev().nth(0)
-            .map(|m| m.insert(name.to_owned(), value))
-            .expect("env is empty");
+        //self.inner.iter_mut().rev().nth(0)
+        //    .map(|m| m.insert(name.to_owned(), value))
+        //    .expect("env is empty");
     }
 
-    // TODO: re-visit closures in ch. 11
-    pub fn split(&mut self) -> Env {
-        let split = self.splits.pop().expect("no closure");
-        let inner = self.inner.iter().skip(split).cloned().collect();
-        Env {
-            inner,
-            splits: Vec::new(),
-        }
-    }
+    //// TODO: re-visit closures in ch. 11
+    //pub fn split(&mut self) -> Env {
+    //    let split = self.splits.pop().expect("no closure");
+    //    let inner = self.inner.iter().skip(split).cloned().collect();
+    //    Env {
+    //        inner,
+    //        splits: Vec::new(),
+    //    }
+    //}
 
-    pub fn children(&self) -> Env {
-        Env {
-            inner: self.inner.iter().skip(1).cloned().collect(),
-            splits: Vec::new(),
-        }
-    }
+    //pub fn children(&self) -> Env {
+    //    Env {
+    //        inner: self.inner.iter().skip(1).cloned().collect(),
+    //        splits: Vec::new(),
+    //    }
+    //}
 }
 
 impl std::fmt::Display for Env {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        let mut s = format!("env: {:?}\n", self.splits);
-        for (n, ctx) in self.inner.iter().rev().enumerate() {
+        let mut s = String::from("env:\n");
+        //let mut s = format!("env: {:?}\n", self.splits);
+        for (n, ctx) in self.inner.borrow().inner.iter().rev().enumerate() {
             s.push_str(&format!("  layer {}\n", n));
             'inner: for (k, v) in ctx.iter() {
                 s.push_str(&format!("    {} = ", k));
