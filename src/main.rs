daeeb21b8926359f8eca53198aefd0415ac35950
Zack <zphixon@gmail.com>
2020-03-23 22:50:26 -0400

    refactor main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,10 @@ use piccolo::chunk::Chunk;
 use piccolo::op::Opcode;
 use piccolo::value::Value;
 
+use piccolo::compiler::Compiler;
+use piccolo::machine::Machine;
+use piccolo::scanner;
+use piccolo::scanner::Scanner;
 use rustyline::error::ReadlineError;
 use rustyline::Editor;
 
@@ -18,15 +22,14 @@ fn main() -> piccolo::Result<()> {
                 Ok(line) => {
                     #[cfg(feature = "pc-debug")]
                     {
-                        let tokens = piccolo::scanner::Scanner::new(&line).scan_tokens()?;
+                        let tokens = Scanner::new(&line).scan_tokens()?;
                         println!("****** tokens");
-                        piccolo::scanner::print_tokens(&tokens);
+                        scanner::print_tokens(&tokens);
                         println!("****** compiler");
-                        let chunk =
-                            piccolo::compiler::Compiler::compile(Chunk::default(), &tokens)?;
+                        let chunk = Compiler::compile(Chunk::default(), &tokens)?;
                         println!("****** chunk");
                         chunk.disassemble("line");
-                        let mut vm = piccolo::machine::Machine::new(chunk);
+                        let mut vm = Machine::new(chunk);
                         println!("****** result");
                         println!("{}", vm.interpret()?);
                     }
@@ -47,55 +50,25 @@ fn main() -> piccolo::Result<()> {
         }
     } else {
         let args: Vec<String> = args.collect();
-        if !args.iter().any(|a| &*a == "pingus") {
-            let contents = std::fs::read_to_string(&args[1]).expect("filename doesn't exist");
-            piccolo::interpret(&contents)?;
-        } else {
-            let mut c = Chunk::default();
+        let contents = std::fs::read_to_string(&args[1]).expect("filename doesn't exist");
 
-            let one = c.constant(Value::Double(1.0));
-            let two = c.constant(Value::Double(2.0));
-            let three = c.constant(Value::Double(3.0));
-            let four = c.constant(Value::Double(4.0));
-            let five = c.constant(Value::Double(5.0));
-            let int = c.constant(Value::Integer(21));
-            let s = c.constant(Value::String("pee".into()));
-
-            c.write(Opcode::Constant, 1);
-            c.write(int as u8, 1);
-            c.write(Opcode::Constant, 1);
-            c.write(one as u8, 1);
-            c.write(Opcode::Add, 1);
-            //c.write(Opcode::Constant, 1);
-            //c.write(one as u8, 1);
-            //c.write(Opcode::Constant, 1);
-            //c.write(int as u8, 1);
-            //c.write(Opcode::Add, 1);
-
-            //c.write(Opcode::Constant, 1);
-            //c.write(one as u8, 1);
-            //c.write(Opcode::Constant, 1);
-            //c.write(two as u8, 1);
-            //c.write(Opcode::Constant, 1);
-            //c.write(three as u8, 1);
-            //c.write(Opcode::Multiply, 1);
-            //c.write(Opcode::Constant, 1);
-            //c.write(four as u8, 1);
-            //c.write(Opcode::Constant, 1);
-            //c.write(five as u8, 1);
-            //c.write(Opcode::Negate, 1);
-            //c.write(Opcode::Divide, 1);
-            //c.write(Opcode::Subtract, 1);
-            //c.write(Opcode::Add, 1);
-            //c.write(Opcode::Negate, 1);
-            //c.write(Opcode::Return, 1);
-
-            c.write(Opcode::Return, 1);
-            c.disassemble("poop");
-
-            let mut vm = piccolo::machine::Machine::new(c);
-            println!(" -- interpret --");
+        #[cfg(feature = "pc-debug")]
+        {
+            let tokens = Scanner::new(&contents).scan_tokens()?;
+            println!("****** tokens");
+            scanner::print_tokens(&tokens);
+            println!("****** compiler");
+            let chunk = Compiler::compile(Chunk::default(), &tokens)?;
+            println!("****** chunk");
+            chunk.disassemble("line");
+            let mut vm = Machine::new(chunk);
+            println!("****** result");
             println!("{}", vm.interpret()?);
+        }
+
+        #[cfg(not(feature = "pc-debug"))]
+        {
+            piccolo::interpret(&contents)?;
         }
     }
 
