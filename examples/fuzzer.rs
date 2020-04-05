b90259e5b84af4c9928a2481864d0c31a1981c68
Zack <zphixon@gmail.com>
2020-04-05 19:49:01 -0400

    refactor fuzzer

--- examples/fuzzer.rs
+++ examples/fuzzer.rs
@@ -1,21 +1,66 @@
 extern crate piccolo;
+extern crate clap;
+
+use clap::{Arg, App, SubCommand};
 
 fn main() {
+    let _ = run();
+}
+
+fn run() -> Result<(), ()> {
     #[cfg(feature = "fuzzer")]
     {
-        let args: Vec<String> = std::env::args().collect();
-        let n = if args.len() == 1 {
-            50
+        let matches = App::new("Piccolo fuzzer")
+            .arg(Arg::with_name("max")
+                .help("exclusive max number of tokens (default 50)")
+                .short("m")
+                .long("max")
+                .takes_value(true))
+            .arg(Arg::with_name("min")
+                .help("inclusive min number of tokens (default 5)")
+                .short("n")
+                .long("min")
+                .takes_value(true))
+            .arg(Arg::with_name("runs")
+                .help("number of runs (default 100)")
+                .index(1))
+            .get_matches();
+
+        let runs = if matches.is_present("runs") {
+            matches.value_of("runs").unwrap().parse::<usize>().map_err(|_| {
+                println!("Expected number");
+            })?
         } else {
-            args[1].parse::<usize>().unwrap_or_else(|_| {
-                println!("expected number, got {}", args[1]);
-                std::process::exit(1);
-            })
+            100
         };
-        piccolo::fuzzer::fuzz(n);
+
+        let min = if matches.is_present("min") {
+            matches.value_of("min").unwrap().parse::<usize>().map_err(|_| {
+                println!("Expected number for min");
+            })?
+        } else {
+            5
+        };
+
+        let max = if matches.is_present("max") {
+            matches.value_of("max").unwrap().parse::<usize>().map_err(|_| {
+                println!("Expected number for max");
+            })?
+        } else {
+            50
+        };
+
+        if min < max {
+            piccolo::fuzzer::fuzz(runs, min, max);
+            Ok(())
+        } else {
+            println!("Expected min < max");
+            Err(())
+        }
     }
     #[cfg(not(feature = "fuzzer"))]
     {
         println!("Fuzzer requires piccolo to be built with --features 'fuzzer'");
+        Err(())
     }
 }
