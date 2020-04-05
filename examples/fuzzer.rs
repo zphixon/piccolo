120dfd42ea426e1b220cb96a04588b7701b2c4ad
Zack <zphixon@gmail.com>
2020-04-05 19:50:04 -0400

    cargo fmt

--- examples/fuzzer.rs
+++ examples/fuzzer.rs
@@ -1,7 +1,7 @@
-extern crate piccolo;
 extern crate clap;
+extern crate piccolo;
 
-use clap::{Arg, App, SubCommand};
+use clap::{App, Arg, SubCommand};
 
 fn main() {
     let _ = run();
@@ -11,41 +11,59 @@ fn run() -> Result<(), ()> {
     #[cfg(feature = "fuzzer")]
     {
         let matches = App::new("Piccolo fuzzer")
-            .arg(Arg::with_name("max")
-                .help("exclusive max number of tokens (default 50)")
-                .short("m")
-                .long("max")
-                .takes_value(true))
-            .arg(Arg::with_name("min")
-                .help("inclusive min number of tokens (default 5)")
-                .short("n")
-                .long("min")
-                .takes_value(true))
-            .arg(Arg::with_name("runs")
-                .help("number of runs (default 100)")
-                .index(1))
+            .arg(
+                Arg::with_name("max")
+                    .help("exclusive max number of tokens (default 50)")
+                    .short("m")
+                    .long("max")
+                    .takes_value(true),
+            )
+            .arg(
+                Arg::with_name("min")
+                    .help("inclusive min number of tokens (default 5)")
+                    .short("n")
+                    .long("min")
+                    .takes_value(true),
+            )
+            .arg(
+                Arg::with_name("runs")
+                    .help("number of runs (default 100)")
+                    .index(1),
+            )
             .get_matches();
 
         let runs = if matches.is_present("runs") {
-            matches.value_of("runs").unwrap().parse::<usize>().map_err(|_| {
-                println!("Expected number");
-            })?
+            matches
+                .value_of("runs")
+                .unwrap()
+                .parse::<usize>()
+                .map_err(|_| {
+                    println!("Expected number");
+                })?
         } else {
             100
         };
 
         let min = if matches.is_present("min") {
-            matches.value_of("min").unwrap().parse::<usize>().map_err(|_| {
-                println!("Expected number for min");
-            })?
+            matches
+                .value_of("min")
+                .unwrap()
+                .parse::<usize>()
+                .map_err(|_| {
+                    println!("Expected number for min");
+                })?
         } else {
             5
         };
 
         let max = if matches.is_present("max") {
-            matches.value_of("max").unwrap().parse::<usize>().map_err(|_| {
-                println!("Expected number for max");
-            })?
+            matches
+                .value_of("max")
+                .unwrap()
+                .parse::<usize>()
+                .map_err(|_| {
+                    println!("Expected number for max");
+                })?
         } else {
             50
         };
