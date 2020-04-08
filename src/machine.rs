b1283cd68b637ca5d5341a63d168ec2f60ad3410
Zack <zphixon@gmail.com>
2020-04-07 22:34:39 -0400

    don't forget to pop the stack after assigning to a global variable

--- src/machine.rs
+++ src/machine.rs
@@ -118,6 +118,7 @@ impl Machine {
                                 },
                                 |_| Ok(()),
                             )?;
+                        self.pop()?;
                         self.ip += 2;
                     }
                 }
