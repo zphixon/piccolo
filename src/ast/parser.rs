94785ca927346e05b67863310d9776be4bbb98a7
Zack <zphixon@gmail.com>
2020-04-13 22:04:33 -0400

    start parser
    I think I'm starting to understand lifetimes :O

---
+++ src/ast/parser.rs
@@ -0,0 +1,32 @@
+use crate::ast::stmt::Stmt;
+use crate::compiler::{scanner::Scanner, Token, TokenKind};
+use crate::error::{ErrorKind, PiccoloError};
+
+pub struct Parser<'a: 'b, 'b> {
+    err: Vec<PiccoloError>,
+    scanner: &'b mut Scanner<'a>,
+}
+
+impl<'a, 'b> Parser<'a, 'b> {
+    pub fn new(scanner: &'b mut Scanner<'a>) -> Self {
+        Parser {
+            err: vec![],
+            scanner,
+        }
+    }
+
+    pub fn parse(mut self) -> Result<Vec<Stmt<'a, 'b>>, Vec<PiccoloError>> {
+        let stmts = vec![
+            Stmt::Retn {
+                keyword: self.scanner.next_token().unwrap(),
+                value: None,
+            }
+        ];
+
+        Ok(stmts)
+    }
+
+    fn current(&self) -> &Token {
+        self.scanner.current()
+    }
+}
