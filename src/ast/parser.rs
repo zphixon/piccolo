4f745d482541a0a3de68dbce43b3cacad174c783
Zack <zphixon@gmail.com>
2020-04-20 13:31:01 -0400

    explicit Parser::consume method

--- src/ast/parser.rs
+++ src/ast/parser.rs
@@ -1,4 +1,4 @@
-use crate::{ErrorKind, PiccoloError, Scanner, TokenKind};
+use crate::{ErrorKind, PiccoloError, Scanner, Token, TokenKind};
 
 use super::Expr;
 use super::Stmt;
@@ -30,24 +30,23 @@ impl<'a> Parser<'a> {
         }
     }
 
-    #[allow(clippy::if_same_then_else)]
     fn declaration(&mut self, scanner: &mut Scanner<'a>) -> Result<(), PiccoloError> {
         if scanner.peek_token(1)?.kind == TokenKind::Assign {
-            let name = scanner.next_token()?;
-            let op = scanner.next_token()?;
+            let name = self.consume(scanner, TokenKind::Identifier)?;
+            let op = self.consume(scanner, TokenKind::Assign)?;
             let value = self.expr_bp(scanner, BindingPower::Assignment)?;
             self.ast.push(Stmt::Assignment { name, op, value });
         } else if scanner.peek_token(1)?.kind == TokenKind::Declare {
-            let name = scanner.next_token()?;
-            let op = scanner.next_token()?;
+            let name = self.consume(scanner, TokenKind::Identifier)?;
+            let op = self.consume(scanner, TokenKind::Declare)?;
             let value = self.expr_bp(scanner, BindingPower::Assignment)?;
             self.ast.push(Stmt::Assignment { name, op, value });
         } else if scanner.peek_token(0)?.kind == TokenKind::Retn {
-            let keyword = scanner.next_token()?;
+            let keyword = self.consume(scanner, TokenKind::Retn)?;
             let value = Some(self.expr_bp(scanner, BindingPower::Assignment)?);
             self.ast.push(Stmt::Retn { keyword, value })
         } else if scanner.peek_token(0)?.kind == TokenKind::Assert {
-            let keyword = scanner.next_token()?;
+            let keyword = self.consume(scanner, TokenKind::Assert)?;
             let value = self.expr_bp(scanner, BindingPower::Assignment)?;
             self.ast.push(Stmt::Assert { keyword, value })
         } else {
@@ -70,13 +69,9 @@ impl<'a> Parser<'a> {
             Expr::Variable(lhs_token)
         } else if lhs_token.kind == TokenKind::LeftParen {
             let e = Expr::Paren(Box::new(self.expr_bp(scanner, BindingPower::Assignment)?));
-            let rp = scanner.next_token()?;
-            if rp.kind != TokenKind::RightParen {
-                return Err(PiccoloError::new(ErrorKind::MalformedExpression {
-                    from: lhs_token.to_string(),
-                })
-                .line(lhs_token.line));
-            }
+            self.consume(scanner, TokenKind::RightParen).map_err(|e| {
+                e.msg_string(format!("in expression starting on line {}", lhs_token.line))
+            })?;
             e
         } else {
             let pbp = prefix_binding_power(lhs_token.kind);
@@ -90,8 +85,7 @@ impl<'a> Parser<'a> {
                 return Err(PiccoloError::new(ErrorKind::MalformedExpression {
                     from: lhs_token.to_string(),
                 })
-                .line(lhs_token.line)
-                .msg("Cannot use as a prefix operator"));
+                .line(lhs_token.line));
             }
         };
 
@@ -117,6 +111,23 @@ impl<'a> Parser<'a> {
         }
 
         Ok(lhs)
+    }
+
+    fn consume(
+        &self,
+        scanner: &mut Scanner<'a>,
+        kind: TokenKind,
+    ) -> Result<Token<'a>, PiccoloError> {
+        let tok = scanner.next_token()?;
+        if tok.kind == kind {
+            Ok(tok)
+        } else {
+            Err(PiccoloError::new(ErrorKind::UnexpectedToken {
+                exp: format!("{:?}", kind),
+                got: format!("{:?}", tok.kind),
+            })
+            .line(tok.line))
+        }
     }
 }
 
