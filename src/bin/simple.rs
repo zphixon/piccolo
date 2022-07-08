8b97695b16f5e3c75d416f999086de182fb27282
Zack <zphixon@gmail.com>
2022-07-07 20:36:16 -0400

    Let simple.rs interpret file

--- src/bin/simple.rs
+++ src/bin/simple.rs
@@ -1,6 +1,21 @@
 use rustyline::Editor;
 
 fn main() {
+    let args = std::env::args().collect::<Vec<_>>();
+    if args.len() > 1 {
+        let src = std::fs::read_to_string(&args[1]).unwrap();
+        match piccolo::interpret(&src) {
+            Ok(constant) => println!("{constant}"),
+            Err(errors) => {
+                for error in errors {
+                    println!("Error: {error}");
+                }
+            }
+        }
+
+        return;
+    }
+
     let mut rl = Editor::<()>::new();
     const PROMPT: &'static str = "> ";
 
